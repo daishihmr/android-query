48dea2a4882434c426b573e3405616da2c686266
daishihmr <daishi.hmr@gmail.com>
2013-12-15 00:33:23 +0900

    add license

--- src/jp/dev7/android/query/Queries.java
+++ src/jp/dev7/android/query/Queries.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 daishi_hmr
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package jp.dev7.android.query;
 
 import java.util.Date;
@@ -53,7 +76,7 @@ public class Queries {
 
     /**
      * 条件式を簡単に作る.
-     * 
+     *
      * @param expression
      *            置き換え文字(?)入りの条件式
      * @param params
@@ -61,6 +84,19 @@ public class Queries {
      */
     public static Selection selection(String expression, Object... params) {
         return new Selection(expression, strings(params));
+    }
+
+    /**
+     * 条件式を簡単に作る.
+     * selectionのエイリアス.
+     *
+     * @param expression
+     *            置き換え文字(?)入りの条件式
+     * @param params
+     *            パラメータ
+     */
+    public static Selection where(String expression, Object... params) {
+        return selection(expression, params);
     }
 
     /**
